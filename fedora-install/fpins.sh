bf54dc43b07b006aedbf3b536949ee77a4bfb6ab
August <august@heen.dev>
2024-05-17 19:30:05 -0500

    Update fpins.sh

--- fedora-install/fpins.sh
+++ fedora-install/fpins.sh
@@ -1,7 +1,5 @@
 flatpak install \
 	com.bitwarden.desktop \
 	com.discordapp.Discord \
-	com.github.iwalton3.jellyfin-media-player \
 	md.obsidian.Obsidian \
-	org.signal.Signal \
-	com.spotify.Client
+	org.signal.Signal
