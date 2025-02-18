ff665bd704b179d86e01fe96a4115061c1b4fbdb
August Heen <august@heen.dev>
2025-02-17 20:50:10 -0600

    Void Atlas 1.3.3 init

---
+++ mc/Void_Atlas-1.3.3.sh
@@ -0,0 +1,614 @@
+# 3d-Skin-Layers [1.7.4]
+packwiz mr install -y https://modrinth.com/mod/zV5r3pPn
+
+# Almanac [1.0.2]
+packwiz mr install -y https://modrinth.com/mod/Gi02250Z
+
+# AmbientSounds [6.1.4]
+packwiz mr install -y https://modrinth.com/mod/fM515JnW
+
+# Amendments [1.20-1.2.16]
+packwiz mr install -y https://modrinth.com/mod/6iTJugQR
+
+# Anvil Restoration [2.3]
+packwiz mr install -y https://modrinth.com/mod/bd8nwTGy
+
+# Apotheosis [7.4.6]
+packwiz mr install -y https://www.curseforge.com/projects/313970
+
+# Apotheosis Artifacts [1.0.1.1-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/GIAFvYa1
+
+# Apotheosis Delight [1.0.3-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/Isgtel6o
+
+# Apotheosis Trade [1.0.0-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/qCh3lNQ3
+
+# Apothic Attributes [1.3.7]
+packwiz mr install -y https://www.curseforge.com/projects/898963
+
+# AppleSkin [2.5.1+mc1.20.1]
+packwiz mr install -y https://modrinth.com/mod/EsAfCjCV
+
+# Architectury [9.2.14]
+packwiz mr install -y https://modrinth.com/mod/lhGA9TYQ
+
+# Ars Additions [1.20.1-1.6.7]
+packwiz mr install -y https://modrinth.com/mod/GYK6Gk8R
+
+# Ars Nouveau [4.12.6]
+packwiz mr install -y https://www.curseforge.com/projects/401955
+
+# Ars Nouveau Artifacts [1.0.2.1-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/ufAUOfNm
+
+# Ars Nouveau Delight [1.1.0.4-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/XjqM4mMx
+
+# Ars Trade [1.0.1.1-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/CYlDHged
+
+# Artifacts [9.5.13]
+packwiz mr install -y https://modrinth.com/mod/P0Mu4wcQ
+
+# Balm [7.3.11]
+packwiz mr install -y https://modrinth.com/mod/MBAkmtvl
+
+# Bamboo Everything [3.0.1-build.55+mc1.20.1]
+packwiz mr install -y https://modrinth.com/mod/cpRkny9R
+
+# Barbeque's Delight [1.0.5]
+packwiz mr install -y https://modrinth.com/mod/rtu7uERF
+
+# BetterF3 [7.0.2]
+packwiz mr install -y https://modrinth.com/mod/8shC1gFX
+
+# BetterThanMending [1.7.2]
+packwiz mr install -y https://modrinth.com/mod/Lvv4SHrK
+
+# Bottle Your Xp [3.4]
+packwiz mr install -y https://modrinth.com/mod/bG2afBot
+
+# Bottled Air [2.4]
+packwiz mr install -y https://modrinth.com/mod/hvrrNux3
+
+# Caelus API [3.2.0+1.20.1]
+packwiz mr install -y https://modrinth.com/mod/40FYwb4z
+
+# Cake Chomps [6.2.0+1.20.1]
+packwiz mr install -y https://modrinth.com/mod/g646EoqQ
+
+# Carry On [2.1.2.7]
+packwiz mr install -y https://modrinth.com/mod/joEfVgkn
+
+# Chat Heads [0.13.11]
+packwiz mr install -y https://modrinth.com/mod/Wb5oqrBJ
+
+# Chefs Delight [1.0.3-forge-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/pvcsfne4
+
+# Cherished Worlds [6.1.7+1.20.1]
+packwiz mr install -y https://modrinth.com/mod/3azQ6p0W
+
+# Chunky [1.3.146]
+packwiz mr install -y https://modrinth.com/mod/fALzjamp
+
+# Cloth Config v10 API [11.1.136]
+packwiz mr install -y https://modrinth.com/mod/9s6osm5g
+
+# Cluttered [2.1]
+packwiz mr install -y https://modrinth.com/mod/UnAEo77V
+
+# Coffee Delight [1.4]
+packwiz mr install -y https://modrinth.com/mod/jn1HiMrY
+
+# Collective [7.87]
+packwiz mr install -y https://modrinth.com/mod/e0M1UDsY
+
+# Comforts [6.4.0+1.20.1]
+packwiz mr install -y https://modrinth.com/mod/SaCpeal4
+
+# Corn Delight [1.1.6-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/uxLAKWU8
+
+# Crabber's Delight [1.1.7d]
+packwiz mr install -y https://modrinth.com/mod/gBGdVBJy
+
+# Crate Delight [24.11.29-1.20-forge]
+packwiz mr install -y https://modrinth.com/mod/9rlXSyLg
+
+# Create [0.5.1.j]
+packwiz mr install -y https://modrinth.com/mod/LNytGWDc
+
+# Create Crafts & Additions [1.20.1-1.2.5]
+packwiz mr install -y https://modrinth.com/mod/kU1G12Nn
+
+# Create Deco [2.0.2-1.20.1-forge]
+packwiz mr install -y https://modrinth.com/mod/sMvUb4Rb
+
+# Create Slice & Dice [3.3.0]
+packwiz mr install -y https://modrinth.com/mod/GmjmRQ0A
+
+# Create Supplementaries [1.0.2.1-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/qtT0lV29
+
+# Create: Central Kitchen [1.3.12]
+packwiz mr install -y https://modrinth.com/mod/btq68HMO
+
+# Create: Interiors [0.5.6]
+packwiz mr install -y https://modrinth.com/mod/r4Knci2k
+
+# Create: Steam 'n' Rails [1.6.7+forge-mc1.20.1]
+packwiz mr install -y https://modrinth.com/mod/ZzjhlDgM
+
+# CreativeCore [2.12.31]
+packwiz mr install -y https://modrinth.com/mod/OsZiaDHq
+
+# Creeper Overhaul [3.0.2]
+packwiz mr install -y https://modrinth.com/mod/MI1LWe93
+
+# Crying Portals [2.8]
+packwiz mr install -y https://modrinth.com/mod/3RkVNglH
+
+# Cuisine Delight [1.1.16]
+packwiz mr install -y https://modrinth.com/mod/gOQjCAAD
+
+# Curios API [5.11.1+1.20.1]
+packwiz mr install -y https://modrinth.com/mod/vvuO3ImH
+
+# Custom Window Title [1.2.0]
+packwiz mr install -y https://www.curseforge.com/projects/360673
+
+# Cycle Paintings [3.6]
+packwiz mr install -y https://modrinth.com/mod/c85whkNB
+
+# Delightful [3.6.2]
+packwiz mr install -y https://modrinth.com/mod/JtSnhtNJ
+
+# Delightful Burgers [1.20.1]
+packwiz mr install -y https://modrinth.com/mod/AqHNT5iP
+
+# Delightful Sandwiches [1.20.1]
+packwiz mr install -y https://modrinth.com/mod/wmyqU46J
+
+# Ding [1.5.0]
+packwiz mr install -y https://modrinth.com/mod/UEtTD3gP
+
+# Display Delight [1.2.0]
+packwiz mr install -y https://modrinth.com/mod/yXepZhp8
+
+# Dynamic Crosshair [9.3]
+packwiz mr install -y https://modrinth.com/mod/ZcR9weSm
+
+# Ears [1.4.6]
+packwiz mr install -y https://modrinth.com/mod/mfzaZK3Z
+
+# Easy Paxel Lite [1.20.1-1.0.3]
+packwiz mr install -y https://www.curseforge.com/projects/476520
+
+# Embeddium [0.3.31+mc1.20.1]
+packwiz mr install -y https://modrinth.com/mod/sk9rgfiA
+
+# End Portal Recipe [5.5]
+packwiz mr install -y https://modrinth.com/mod/VbCjkvcq
+
+# End's Delight [2.5]
+packwiz mr install -y https://modrinth.com/mod/yHN0njMr
+
+# Enigmatic Legacy [2.30.1]
+packwiz mr install -y https://modrinth.com/mod/IpUWAlqM
+
+# Enigmatic Legacy Delight [1.1.0-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/4kibTwHn
+
+# EntityCulling [1.7.2]
+packwiz mr install -y https://modrinth.com/mod/NNAgCjsB
+
+# Explorer's Compass [1.20.1-1.3.3-forge]
+packwiz mr install -y https://modrinth.com/mod/RV1qfVQ8
+
+# Extract Poison [3.4]
+packwiz mr install -y https://modrinth.com/mod/S4eFpJFD
+
+# Fairy Lights [7.0.0]
+packwiz mr install -y https://www.curseforge.com/projects/233342
+
+# FallingTree [4.3.4]
+packwiz mr install -y https://modrinth.com/mod/Fb4jn8m6
+
+# Farmer's Delight [1.20.1-1.2.6]
+packwiz mr install -y https://modrinth.com/mod/R2OftAxM
+
+# Ferrite Core [6.0.1]
+packwiz mr install -y https://modrinth.com/mod/uXXizFIs
+
+# Festive Delight [1.3.0]
+packwiz mr install -y https://modrinth.com/mod/AsUUCoXX
+
+# Fish On The Line [3.4]
+packwiz mr install -y https://modrinth.com/mod/R6PYx1PW
+
+# Fixed Anvil Repair Cost [3.4]
+packwiz mr install -y https://modrinth.com/mod/jmLyNFBG
+
+# Followers Teleport Too [2.6]
+packwiz mr install -y https://modrinth.com/mod/E5YVNsbH
+
+# Forgiving Void [10.0.1]
+packwiz mr install -y https://modrinth.com/mod/1vkzEZjE
+
+# Friends & Foes [3.0.7]
+packwiz mr install -y https://modrinth.com/mod/BOCJKD49
+
+# Friends&Foes - Beekeeper Hut [2.0.1]
+packwiz mr install -y https://modrinth.com/mod/K62sN7Sd
+
+# Friends&Foes - Flowery Mooblooms [2.0.2]
+packwiz mr install -y https://modrinth.com/mod/wYtDgIUZ
+
+# Fruits Delight [1.0.16]
+packwiz mr install -y https://modrinth.com/mod/g6sbyCTu
+
+# Furnace Recycle [2.5]
+packwiz mr install -y https://modrinth.com/mod/HyKmtI3L
+
+# GeckoLib 4 [4.4.9]
+packwiz mr install -y https://modrinth.com/mod/8BmcQJ2H
+
+# Grass Seeds [3.3]
+packwiz mr install -y https://modrinth.com/mod/Y6d4uRJn
+
+# Guard Villagers [1.20.1-1.6.10]
+packwiz mr install -y https://modrinth.com/mod/H1sntfo8
+
+# HajCraft [1.0.0]
+packwiz mr install -y https://modrinth.com/mod/K1YKPXzI
+
+# Handcrafted [3.0.6]
+packwiz mr install -y https://modrinth.com/mod/pJmCFF0p
+
+# Healing Campfire [6.1]
+packwiz mr install -y https://modrinth.com/mod/kOuPUitF
+
+# Honey Delight [1.0.2.1-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/bDaCR2oG
+
+# Iceberg [1.1.13]
+packwiz mr install -y https://modrinth.com/mod/5faXoLqX
+
+# Improved Sign Editing [1.4]
+packwiz mr install -y https://modrinth.com/mod/EWQifKYI
+
+# Incendium [5.3.5]
+packwiz mr install -y https://modrinth.com/mod/ZVzW5oNS
+
+# Inventory Profiles Next [1.10.14]
+packwiz mr install -y https://modrinth.com/mod/O7RBXm3n
+
+# JRFTL [1.6.0]
+packwiz mr install -y https://modrinth.com/mod/8nIicBYu
+
+# Jade [11.12.3+forge]
+packwiz mr install -y https://modrinth.com/mod/nvQzSEkH
+
+# Jade Addons [5.3.1+forge]
+packwiz mr install -y https://modrinth.com/mod/xuDOzCLy
+
+# Just Enough Advancements [1.20.1-5.0.1]
+packwiz mr install -y https://modrinth.com/mod/5JY3QNIB
+
+# Just Enough Archaeology [1.20.1-1.0.5]
+packwiz mr install -y https://modrinth.com/mod/K0LQpdmm
+
+# Just Enough Breeding [1.5.0]
+packwiz mr install -y https://modrinth.com/mod/9Pk89J3g
+
+# Just Enough Effects Descriptions [1.20-2.2.5]
+packwiz mr install -y https://modrinth.com/mod/EO27GKs1
+
+# Just Enough Items [15.20.0.106]
+packwiz mr install -y https://modrinth.com/mod/u6dRKJwZ
+
+# Just Enough Professions (JEP) [3.0.1]
+packwiz mr install -y https://modrinth.com/mod/kB56GtWA
+
+# Just Enough Resources [1.4.0.247]
+packwiz mr install -y https://modrinth.com/mod/uEfK2CXF
+
+# Keep Some Inventory [1.3.3]
+packwiz mr install -y https://modrinth.com/mod/VHSdL301
+
+# Kotlin for Forge [hmeyC41q]
+packwiz mr install -y https://modrinth.com/mod/ordsPcFz
+
+# Legendary Tooltips [1.4.5]
+packwiz mr install -y https://modrinth.com/mod/atHH8NyV
+
+# Let Me Despawn [1.4.4]
+packwiz mr install -y https://modrinth.com/mod/vE2FN5qn
+
+# LibX [1.20.1-5.0.14]
+packwiz mr install -y https://modrinth.com/mod/qEH6GYul
+
+# Macaw's Bridges [3.0.0]
+packwiz mr install -y https://modrinth.com/mod/GURcjz8O
+
+# Macaw's Doors [1.1.2]
+packwiz mr install -y https://modrinth.com/mod/kNxa8z3e
+
+# Macaw's Fences and Walls [1.1.2]
+packwiz mr install -y https://modrinth.com/mod/GmwLse2I
+
+# Macaw's Furniture [3.3.0]
+packwiz mr install -y https://modrinth.com/mod/dtWC90iB
+
+# Macaw's Holidays [1.1.0]
+packwiz mr install -y https://modrinth.com/mod/rH20L2Lp
+
+# Macaw's Lights and Lamps [1.1.0]
+packwiz mr install -y https://modrinth.com/mod/w4an97C2
+
+# Macaw's Paintings [1.0.5]
+packwiz mr install -y https://modrinth.com/mod/okE6QVAY
+
+# Macaw's Paths and Pavings [1.0.5]
+packwiz mr install -y https://modrinth.com/mod/VRLhWB91
+
+# Macaw's Roofs [2.3.1]
+packwiz mr install -y https://modrinth.com/mod/B8jaH3P1
+
+# Macaw's Stairs and Balconies [1.0.0]
+packwiz mr install -y https://modrinth.com/mod/iP3wH1ha
+
+# Macaw's Trapdoors [1.1.4]
+packwiz mr install -y https://modrinth.com/mod/n2fvCDlM
+
+# Macaw's Windows [2.3.0]
+packwiz mr install -y https://modrinth.com/mod/C7I0BCni
+
+# Miner's Delight [1.20.1-1.2.3]
+packwiz mr install -y https://modrinth.com/mod/qMxbM4BQ
+
+# ModernFix [5.20.2+mc1.20.1]
+packwiz mr install -y https://modrinth.com/mod/nmDcB62a
+
+# Moonlight Library [1.20-2.13.58]
+packwiz mr install -y https://modrinth.com/mod/twkfQtEc
+
+# More Delight [25.01.13-1.20-forge]
+packwiz mr install -y https://modrinth.com/mod/znHQQtuU
+
+# Mysterious Mountain Lib [1.5.17-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/ntMyNH8c
+
+# Name Tag Tweaks [3.8]
+packwiz mr install -y https://modrinth.com/mod/LrLZEnPl
+
+# Naturalist [4.0.3]
+packwiz mr install -y https://modrinth.com/mod/F8BQNPWX
+
+# Nature's Compass [1.20.1-1.11.2-forge]
+packwiz mr install -y https://modrinth.com/mod/fPetb5Kh
+
+# Natures Aura Delight [1.1.0-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/i1z52iUm
+
+# NaturesAura [39.4]
+packwiz mr install -y https://modrinth.com/mod/4cJkN3aF
+
+# Nether's Delight [1.20.1-4.0]
+packwiz mr install -y https://modrinth.com/mod/Vv0RM7WN
+
+# No Animal Tempt Delay [1.2]
+packwiz mr install -y https://modrinth.com/mod/7YM5AXYm
+
+# No Feather Trample [1.3]
+packwiz mr install -y https://modrinth.com/mod/VmGOLJeH
+
+# Not Enough Recipe Book [0.4.1]
+packwiz mr install -y https://modrinth.com/mod/bQh7xzFq
+
+# Nullscape [1.2.8]
+packwiz mr install -y https://modrinth.com/mod/LPjGiSO4
+
+# Ocean's Delight [1.0.2-1.20]
+packwiz mr install -y https://modrinth.com/mod/DGiq4ZSW
+
+# OctoLib [0.4.2]
+packwiz mr install -y https://modrinth.com/mod/RH2KUdKJ
+
+# Oculus [1.8.0]
+packwiz mr install -y https://modrinth.com/mod/GchcoXML
+
+# Oculus Flywheel Compat [1.1.4]
+packwiz mr install -y https://modrinth.com/mod/ndHYMY2K
+
+# Paper Books [3.5]
+packwiz mr install -y https://modrinth.com/mod/QM2wt9X3
+
+# Patchouli [1.20.1-84-FORGE]
+packwiz mr install -y https://modrinth.com/mod/nU0bVIaL
+
+# Pizza Delight [1.0.1-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/Cb5J3sNW
+
+# Placebo [8.6.2]
+packwiz mr install -y https://www.curseforge.com/projects/283644
+
+# Polymorph [0.49.8+1.20.1]
+packwiz mr install -y https://modrinth.com/mod/tagwiZkJ
+
+# PrettyPipes [1.20.1]
+packwiz mr install -y https://modrinth.com/mod/XExdLWCQ
+
+# Prism [1.0.5]
+packwiz mr install -y https://modrinth.com/mod/1OE8wbN0
+
+# Puzzles Lib [8.1.25]
+packwiz mr install -y https://modrinth.com/mod/QAGBst4M
+
+# Replanting Crops [5.4]
+packwiz mr install -y https://modrinth.com/mod/EXzIPtJo
+
+# Resourceful Lib [2.1.29]
+packwiz mr install -y https://modrinth.com/mod/G1hIVOrD
+
+# Resourcefulconfig [2.1.2]
+packwiz mr install -y https://modrinth.com/mod/M1953qlQ
+
+# Respawning Shulkers [4.0]
+packwiz mr install -y https://modrinth.com/mod/gHCmhGUV
+
+# Rustic Delight [1.3.2]
+packwiz mr install -y https://modrinth.com/mod/foa4fGIH
+
+# Shulker Drops Two [3.4]
+packwiz mr install -y https://modrinth.com/mod/UjXIyw47
+
+# Simple Voice Chat [1.20.1-2.5.27]
+packwiz mr install -y https://modrinth.com/mod/9eGKb6K1
+
+# Sit [1.3.5]
+packwiz mr install -y https://modrinth.com/mod/VKXzIykF
+
+# Sophisticated Backpacks [3.23.2.1186]
+packwiz mr install -y https://modrinth.com/mod/TyCTlI4b
+
+# Sophisticated Core [1.2.2.847]
+packwiz mr install -y https://modrinth.com/mod/nmoqTijg
+
+# Sophisticated Storage [1.3.1.1050]
+packwiz mr install -y https://modrinth.com/mod/hMlaZH8f
+
+# Sound Physics Remastered [1.20.1-1.4.8]
+packwiz mr install -y https://modrinth.com/mod/qyVF9oeo
+
+# SquakeReforged [1.1]
+packwiz mr install -y https://modrinth.com/mod/5B0Ejhu3
+
+# Stoneworks [8.0.0]
+packwiz mr install -y https://modrinth.com/mod/FzyTKtVF
+
+# Storage Delight [24.12.15-1.20-forge]
+packwiz mr install -y https://modrinth.com/mod/LTTvOp5L
+
+# Storage Drawers [12.9.13]
+packwiz mr install -y https://modrinth.com/mod/guitPqEi
+
+# Supplementaries [1.20-3.1.12]
+packwiz mr install -y https://modrinth.com/mod/fFEIiSDQ
+
+# Supplementaries Delight [1.1.0-1.20.1]
+packwiz mr install -y https://modrinth.com/mod/SivjJhqG
+
+# Supplementaries Squared [1.20-1.1.18]
+packwiz mr install -y https://modrinth.com/mod/dCCkNFwE
+
+# Tax Free Levels [1.4.7]
+packwiz mr install -y https://modrinth.com/mod/jCBrrLTs
+
+# The Official Bingus Mod [2.0.0]
+packwiz mr install -y https://modrinth.com/mod/VdEsgz29
+
+# Thorny Bush Protection [1.4]
+packwiz mr install -y https://modrinth.com/mod/hYGSXIE7
+
+# Tool Belt [1.20.02]
+packwiz mr install -y https://www.curseforge.com/projects/260262
+
+# Tool Trims [2.2.1]
+packwiz mr install -y https://modrinth.com/mod/uXeEiQk1
+
+# Ube's Delight [0.3.2]
+packwiz mr install -y https://modrinth.com/mod/PYjpoAys
+
+# Universal Sawmill [1.20-1.4.6]
+packwiz mr install -y https://modrinth.com/mod/WRaRZdTd
+
+# Vampire's Delight [0.1.7c]
+packwiz mr install -y https://modrinth.com/mod/9Vo2deFq
+
+# Vampirism [1.10.12]
+packwiz mr install -y https://modrinth.com/mod/jVZ0F1wn
+
+# Vegan Delight [1.4]
+packwiz mr install -y https://modrinth.com/mod/Na8nA0jA
+
+# Veggies Delight [1.6]
+packwiz mr install -y https://modrinth.com/mod/emQ94xri
+
+# Village Bell Recipe [3.6]
+packwiz mr install -y https://modrinth.com/mod/u692zai1
+
+# Vintage Delight [0.1.6]
+packwiz mr install -y https://modrinth.com/mod/T7XZLHGD
+
+# Xaero's Minimap [25.0.0]
+packwiz mr install -y https://modrinth.com/mod/1bokaNcj
+
+# Xaero's World Map [1.39.2]
+packwiz mr install -y https://modrinth.com/mod/NcUtCpym
+
+# YUNG's API [1.20-Forge-4.0.6]
+packwiz mr install -y https://modrinth.com/mod/Ua7DFN59
+
+# YUNG's Better Desert Temples [1.20-Forge-3.0.3]
+packwiz mr install -y https://modrinth.com/mod/XNlO7sBv
+
+# YUNG's Better Dungeons [1.20-Forge-4.0.4]
+packwiz mr install -y https://modrinth.com/mod/o1C1Dkj5
+
+# YUNG's Better Jungle Temples [1.20-Forge-2.0.5]
+packwiz mr install -y https://modrinth.com/mod/z9Ve58Ih
+
+# YUNG's Better Mineshafts [1.20-Forge-4.0.4]
+packwiz mr install -y https://modrinth.com/mod/HjmxVlSr
+
+# YUNG's Better Nether Fortresses [1.20-Forge-2.0.6]
+packwiz mr install -y https://modrinth.com/mod/Z2mXHnxP
+
+# YUNG's Better Ocean Monuments [1.20-Forge-3.0.4]
+packwiz mr install -y https://modrinth.com/mod/3dT9sgt4
+
+# YUNG's Better Strongholds [1.20-Forge-4.0.3]
+packwiz mr install -y https://modrinth.com/mod/kidLKymU
+
+# YUNG's Better Witch Huts [1.20-Forge-3.0.3]
+packwiz mr install -y https://modrinth.com/mod/t5FRdP87
+
+# YUNG's Bridges [1.20-Forge-4.0.3]
+packwiz mr install -y https://modrinth.com/mod/Ht4BfYp6
+
+# Zombie Villagers From Spawner [3.9]
+packwiz mr install -y https://modrinth.com/mod/wQlT8uIq
+
+# [Let's Do] API [1.2.15]
+packwiz mr install -y https://modrinth.com/mod/4XJZeZbM
+
+# [Let's Do] Bakery [1.1.15]
+packwiz mr install -y https://modrinth.com/mod/WFwYiVoG
+
+# [Let's Do] Brewery [1.1.9]
+packwiz mr install -y https://modrinth.com/mod/cV5LQXKx
+
+# [Let's Do] Candlelight [1.2.13]
+packwiz mr install -y https://modrinth.com/mod/mPND0Adp
+
+# [Let's Do] Furniture [1.0.4]
+packwiz mr install -y https://modrinth.com/mod/3JQDJrYW
+
+# [Let's Do] HerbalBrews [1.0.10]
+packwiz mr install -y https://modrinth.com/mod/Eh11TaTm
+
+# [Let's Do] Meadow [1.3.22]
+packwiz mr install -y https://modrinth.com/mod/ra7o6Sl7
+
+# [Let's Do] Wilder Nature [1.0.4]
+packwiz mr install -y https://modrinth.com/mod/AAiRU4aQ
+
+# iChunUtil [1.0.3]
+packwiz mr install -y https://modrinth.com/mod/W6ROj0Hl
+
+# libIPN [4.0.2]
+packwiz mr install -y https://modrinth.com/mod/onSQdWhM
